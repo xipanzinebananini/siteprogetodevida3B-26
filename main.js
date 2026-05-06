f6321c499157c6b493fdcf9f78429e2de4630552
xipanzinebananini <matheus.cunha.bento@escola.pr.gov.br>
2026-05-06 12:19:23 -0300

    aula 3

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const botoes = document.querySelectorAll(".botao");
 
 for(let i =0; i < botoes.length; i++){
     botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++){
+        for (let j = 0; j < botoes.length; j++) {
 
         }
-    }
+
 botoes [i].classList.add( "ativo");{
 ;}
 }
