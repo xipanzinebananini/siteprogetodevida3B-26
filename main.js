5c9bc4033f30e28dfbaeee635b491ff2a907d82a
xipanzinebananini <matheus.cunha.bento@escola.pr.gov.br>
2026-06-10 12:11:27 -0300

    1006

--- main.js
+++ main.js
@@ -16,5 +16,19 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-10-10T00:00:00");
-let tempoAtual = new Date()
+let tempoAtual = new Date();
 
+contadores[0].textContent = CalculaTempo(tempoObjetivo1);
+function CalculaTempo(tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
